d1065aab71d70e6826b0c109587c2d99c5f7347b
Bartol Deak <b@bdeak.net>
2022-05-12 00:08:55 +0200

    disable button while waiting for contact form response

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const messages = {
   "message-required": "Poruka je obavezna",
   "email-not-valid": "Email nije validan",
   "mail-sent": "Poruka uspješno poslana",
+  "button-sending": "Poruka se šalje...",
+  "button-default": "Pošalji",
 };
 
 document
   .querySelector(".contact__form")
   .addEventListener("submit", async function (event) {
     event.preventDefault();
+
+    const submitButtonElement = document.querySelector(".contact__button");
+    submitButtonElement.innerHTML = messages["button-sending"];
+    submitButtonElement.disabled = true;
 
     const response = await fetch(
       "https://sr2k3rjwcgfezjuq4b7fgagn3m0hqgal.lambda-url.eu-central-1.on.aws/",
@@ -30,4 +36,7 @@ document
     messageElement.innerHTML = messages[message];
     messageElement.classList.add(response.ok ? "green" : "red");
     messageElement.classList.remove(!response.ok ? "green" : "red");
+
+    submitButtonElement.innerHTML = messages["button-default"];
+    submitButtonElement.disabled = false;
   });
